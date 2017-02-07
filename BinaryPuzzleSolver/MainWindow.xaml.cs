c1e9a75b88b02e9cb24c76f150bd21d0b510558c
sudpuzzer <braydon.g@live.com>
2017-02-06 20:44:54 -0600

    Initial commit

--- BinaryPuzzleSolver/MainWindow.xaml.cs
+++ BinaryPuzzleSolver/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,49 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace BinaryPuzzleSolver
 {
+public class puzzleSize : INotifyPropertyChanged
+    {
+        private int size;
+        public int Size
+        {
+            get { return this.size; }
+            set
+            {
+                if(this.size != value)
+                {
+                    this.size = value;
+                    this.NotifyPropertyChanged("Size");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private puzzleSize puzzle = new puzzleSize() { Size = 1 };
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void puzzleSizeSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            puzzle.Size = 2;
         }
     }
+    
 }
---
+++ BinaryPuzzleSolver/Pieces.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryPuzzleSolver
+{
+    public class Pieces
+    {
+        //Create array which will identify the puzzle pieces
+        //Side: 1 = left, 2 = top, 3 = right, 4 = bottom
+        //For each side, described as follows:
+        //1 = color1, half1
+        //2 = color1, half2
+        //3 = color2, half1
+        //4 = color2, half2
+        //5 = color3, half1
+        //6 = color3, half2
+        //7 = color4, half1
+        //8 = color4, half2
+        public Pieces(int size)
+        {
+            int numberOfPieces = size * size;
+            int[,] pieces = new int[numberOfPieces, 4];
+        }
+
+    }
+    public class Colors
+    {
+        //Defines the colors of the puzzle pieces
+        //1 = White
+        //2 = Black
+        //3 = Grey
+        //4 = Red
+        //5 = Green
+        //6 = Blue
+        //7 = Yellow
+        //8 = Orange
+        //9 = Purple
+        //10 = Indigo
+
+        int color1;
+        int color2;
+        int color3;
+        int color4;
+    }
+}
